bd0c6055236b93d7cf6cfa156641eb3c3a786568
lucas <lferreira_prest@telecine.com.br>
2019-10-27 03:06:41 -0300

    feat(core): file cache added
    added option to use file cache

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+class NoCache(Exception):
+    pass
+
+
 class HappyOrNot:
     """ this class share atribbute for the happy validator """
     def __init__(self, cache_file: str = '', cache: bool = True) -> None:
-        pass
+        self.cache = None
+        if not cache and cache_file:
+            raise NoCache("cache file need cache option active")
+        if cache:
+            self.cache = {0: False}
+        self.cache_file = None
+        if cache_file:
+            self.cache_file = cache_file
+            with open(cache_file, 'a+') as f:
+                f.seek(0)
+                for line in f.readlines():
+                    number, boolean, _ = line.split(' ')
+                    self.cache[int(number)] = eval(boolean)
 
     def happy_or_not(self, number: int) -> bool:
         """ the function to test for happy numbers
@@ -13,15 +29,49 @@ class HappyOrNot:
         RETURN: TYPE
         ------ -----
             bool
-                a boolean to indicate true if happya nd false if not
+                a boolean to indicate true if happy and false if not
         """
         if number == 0:
             return False
+        if self.cache:
+            if number in self.cache:
+                return self.cache[number]
+            stacked = set()
+            stacked.add(number)
+        square_number = number
+        phase_square_number = self.sum_square_number(number)
+        while square_number != phase_square_number:
+            square_number = self.sum_square_number(square_number)
+            phase_square_number = self.sum_square_number(phase_square_number)
+            phase_square_number = self.sum_square_number(phase_square_number)
+            if self.cache:
+                stacked.add(square_number)
+        if self.cache:
+            for stack in stacked:
+                self.cache[stack] = square_number == 1
+            self.update_cache_file()
+        return square_number == 1
 
+    def update_cache_file(self):
+        if self.cache_file:
+            with open(self.cache_file, 'w') as f:
+                for stack, value in sorted(self.cache.items()):
+                    print(f'{stack} {value} \n')
+                    f.write(f'{stack} {value} \n')
+
+    @staticmethod
+    def sum_square_number(number: int) -> int:
+        """ the function to square and sum the actual number
+        PARAM : TYPE
+        -----   ----
+        number: int
+            the number to be squared
+
+        RETURN : TYPE
+        ------ -----
+            int
+                the squared number
+        """
         # doing string conversion help with not doing division and then mod operator,
         # for large numbrs helps in time complexity
-        sum_square_number = 0
-        while sum_square_number != 1:
-            sum_square_number = sum([int(n)**2 for n in str(number)])
-            if sum_square_number == 1:
-                return True
+        return sum([int(n)**2 for n in str(number)])
--- tests/test_happy.py
+++ tests/test_happy.py
@@ -1,12 +1,45 @@
 import unittest
 
-from main import HappyOrNot
+from main import HappyOrNot, NoCache
 
 
 class HappyTest(unittest.TestCase):
-    def test_happy(self):
-        with self.subTest("get correct happy number"):
+    def test_happy_numbers_without_memory_cache(self):
+        happy_class = HappyOrNot(cache=False)
+        with self.subTest("get true with correct happy numbers"):
             happy_numbers = [49, 97, 130, 10]
             for happy in happy_numbers:
-                self.assertTrue(HappyOrNot(happy))
+                self.assertTrue(happy_class.happy_or_not(happy))
 
+        with self.subTest("get false with incorrect happy numbers"):
+            happy_numbers = [0, 2, 3, 4, 5, 6]
+            for happy in happy_numbers:
+                self.assertFalse(happy_class.happy_or_not(happy))
+
+    def test_happy_numbers_with_memory_cache(self):
+        happy_class = HappyOrNot(cache=True)
+        with self.subTest("get true with correct happy numbers"):
+            happy_numbers = [49, 49, 97, 130, 10]
+            for happy in happy_numbers:
+                self.assertTrue(happy_class.happy_or_not(happy))
+
+        with self.subTest("get false with incorrect happy numbers"):
+            happy_numbers = [0, 2, 2, 3, 4, 5, 6]
+            for happy in happy_numbers:
+                self.assertFalse(happy_class.happy_or_not(happy))
+
+    def test_happy_numbers_with_file_cache(self):
+        happy_class = HappyOrNot(cache=True, cache_file='number.dat')
+        with self.subTest("get true with correct happy numbers"):
+            happy_numbers = [49, 49, 97, 130, 10]
+            for happy in happy_numbers:
+                self.assertTrue(happy_class.happy_or_not(happy))
+
+        with self.subTest("get false with incorrect happy numbers"):
+            happy_numbers = [0, 2, 2, 3, 4, 5, 6]
+            for happy in happy_numbers:
+                self.assertFalse(happy_class.happy_or_not(happy))
+
+        with self.subTest("get error when no cache is set"):
+            with self.assertRaises(NoCache):
+                happy_class = HappyOrNot(cache=False, cache_file='number.dat')
